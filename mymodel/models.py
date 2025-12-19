081375c19bde2f2acebd3b85a8a7efdca6d2602d
Mostafa Raihan <raihan.invite@gmail.com>
2025-12-19 21:50:33 +0600

    database create

--- mymodel/models.py
+++ mymodel/models.py
@@ -4,7 +4,7 @@ class MyModel(models.Model):
     name = models.CharField(max_length=100, blank=False, null=False)
     age = models.IntegerField(blank=False, null=False)
     email = models.EmailField(unique=True, blank=False, null=False)
-    phone = models.IntegerField(max_length=15, blank=False, null=False)
+    phone = models.IntegerField(blank=False, null=False)
     salary = models.DecimalField(max_digits=10, decimal_places=2, blank=False, null=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
