b86d40f4cb1dcb5e88dae270ab2040f602a5255f
Mostafa Raihan <raihan.invite@gmail.com>
2025-12-19 21:39:13 +0600

    django project

---
+++ Django_Model_validation/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for Django_Model_validation project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/6.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Django_Model_validation.settings')
+
+application = get_asgi_application()
