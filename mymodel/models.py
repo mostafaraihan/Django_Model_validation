66541adc3f1454acd611026f466ec5f4b71207c5
Mostafa Raihan <raihan.invite@gmail.com>
2025-12-19 21:48:15 +0600

    database model create

--- mymodel/models.py
+++ mymodel/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class MyModel(models.Model):
+    name = models.CharField(max_length=100, blank=False, null=False)
+    age = models.IntegerField(blank=False, null=False)
+    email = models.EmailField(unique=True, blank=False, null=False)
+    phone = models.IntegerField(max_length=15, blank=False, null=False)
+    salary = models.DecimalField(max_digits=10, decimal_places=2, blank=False, null=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
